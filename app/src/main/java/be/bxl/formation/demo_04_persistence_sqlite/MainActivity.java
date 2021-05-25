9e15d5175453dfbe55a6462148a64d4d42fa6142
Aurelien <aurelien.strimelle@bstorm.be>
2021-05-25 09:59:56 -0700

    Ajout d'une demo d'utilisation

--- app/src/main/java/be/bxl/formation/demo_04_persistence_sqlite/MainActivity.java
+++ app/src/main/java/be/bxl/formation/demo_04_persistence_sqlite/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import be.bxl.formation.demo_04_persistence_sqlite.db.dao.ProductDao;
+import be.bxl.formation.demo_04_persistence_sqlite.models.Product;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Exemple d'utilisation de la base de donnée dans une activité
+        //  - Initialisation du DAO pour travailler avec des produits
+        ProductDao productDao = new ProductDao(getApplicationContext());
+
+        //  - Création d'un produit
+        Product p1 = new Product("Demo", "N/A", 2, 3.14);
+
+        productDao.openWritable();
+        long id = productDao.insert(p1);
+        productDao.close();
+
+        //  - Lecture de tout les produits en DB
+        productDao.openReadable();
+        List<Product> products = productDao.getAll();
+        productDao.close();
+
+        // - Lecture du produit avec l'id de valeur "1"
+        productDao.openReadable();
+        Product p2 = productDao.getById(1);
+        productDao.close();
     }
 }
